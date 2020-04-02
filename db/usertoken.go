c1928d8b6028458280def627a57be1f7466a6220
rh01 <1048157315@qq.com>
2020-04-02 17:05:44 +0800

    ignore ci error

---
+++ db/usertoken.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"fmt"
+
+	mydb "github.com/rh01/oss-storage/db/mysql"
+)
+
+// UpdateToken : 更新用户tokene
+func UpdateToken(username string, token string) bool {
+	stmt, err := mydb.DBConn().Prepare(
+		"replace into tbl_user_token (`user_name`,`user_token`) values (?, ?)")
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(username, token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	return true
+}
